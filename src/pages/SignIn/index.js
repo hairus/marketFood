b0fa8024cba928d171f91e6b7a795a45aa639fac
hairus sabilah <ajisabilah10032010@gmail.com>
2022-09-22 22:29:57 +0700

    sign up

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Button, Gap, Header, TextInput } from '../../components'
+
+const SignIn = ({navigation}) => {
+  return (
+    <View style={styles.page}>
+        <Header title="SIGN IN" subTitle="Find your Best ever Meal" />
+        <View style={styles.container}>
+            <TextInput label="Email" placeholder="enter your Email" />
+            <Gap height={16} />
+            <TextInput label="Password" placeholder="enter your password" />
+            <Gap height={24} />
+            <Button text="SIGN IN"/>
+            <Gap height={12} />
+            <Button text="Create New Account" color="#8D92A3" textColor="white" onPress={()=> navigation.navigate('SignUp')} />
+        </View>
+    </View>
+  )
+}
+
+export default SignIn
+
+const styles = StyleSheet.create({
+    page:{flex:1},
+    container:{backgroundColor:'white', paddingHorizontal:24, paddingVertical:26, marginTop:24, flex:1},
+})
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import SplashScreen from "./SplashScreen";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp/index.";
+export { SplashScreen, SignIn, SignUp }
